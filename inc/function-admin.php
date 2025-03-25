c16e1507ff7a16ea7e13509ddd7c4335c6721ba8
Hoang Nam <vannamhh@gmail.com>
2025-03-25 11:24:45 +0700

    chore: create a custome admin page

---
+++ inc/function-admin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package sunset-classic-theme
+ */
+function sunset_add_admin_page() {
+	add_menu_page(
+		'Sunset Theme Options',
+		'Sunset',
+		'manage_options',
+		'vn-sunset',
+		'sunset_theme_create_page',
+		get_template_directory_uri() . '/img/sunset-icon.png',
+		110
+	);
+}
+add_action( 'admin_menu', 'sunset_add_admin_page' );
+
+
+function sunset_theme_create_page() {
+	// generation of our admin page.
+  echo '<h1>Sunset Theme Options</h1>';
+}
