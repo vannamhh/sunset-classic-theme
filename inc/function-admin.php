3fbd6ad7a8c11980b3a17339c59b8d30fe4933e9
Hoang Nam <vannamhh@gmail.com>
2025-03-25 13:01:42 +0700

    feat(admin): add theme options panel with subpages

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -3,14 +3,34 @@
  * @package sunset-classic-theme
  */
 function sunset_add_admin_page() {
+	// Generate Sunset Admin Page.
 	add_menu_page(
 		'Sunset Theme Options',
 		'Sunset',
 		'manage_options',
-		'vn-sunset',
+		'vn_sunset',
 		'sunset_theme_create_page',
 		get_template_directory_uri() . '/img/sunset-icon.png',
 		110
+	);
+
+	// Generate Sunset Admin Sub Pages.
+	add_submenu_page(
+		'vn_sunset',
+		'Sunset Theme Options',
+		'General',
+		'manage_options',
+		'vn_sunset',
+		'sunset_theme_create_page'
+	);
+
+	add_submenu_page(
+		'vn_sunset',
+		'Sunset Setting',
+		'Setting',
+		'manage_options',
+		'vn_sunset_setting',
+		'sunset_theme_settings_page'
 	);
 }
 add_action( 'admin_menu', 'sunset_add_admin_page' );
@@ -18,5 +38,8 @@ add_action( 'admin_menu', 'sunset_add_admin_page' );
 
 function sunset_theme_create_page() {
 	// generation of our admin page.
-  echo '<h1>Sunset Theme Options</h1>';
+  require_once( get_template_directory() . '/inc/templates/sunset-admin.php');
+}
+
+function sunset_theme_settings_page() {
 }
