e646c141e3474f29cb8d46a28135d9510fb4d8d0
Hoang Nam <vannamhh@gmail.com>
2025-03-26 18:22:19 +0700

    feat(metabox): create a custom meta box

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -6,8 +6,12 @@
 $activate_form = get_option( 'activate_form' );
 if ( isset( $activate_form ) && '1' === $activate_form ) {
 	add_action( 'init', 'sunset_contact_custom_post_type' );
+
 	add_filter( 'manage_sunset-contact_posts_columns', 'sunset_set_contact_columns' );
-	add_filter( 'manage_sunset-contact_posts_custom_column', 'sunset_contact_custom_column', 10, 2 );
+	add_action( 'manage_sunset-contact_posts_custom_column', 'sunset_contact_custom_column', 10, 2 );
+
+	add_action( 'add_meta_boxes', 'sunset_contact_add_meta_box' );
+	add_action( 'save_post', 'sunset_save_contact_email_data' );
 }
 
 /** Contact CPT */
@@ -45,13 +49,59 @@ function sunset_set_contact_columns( $columns ) {
 function sunset_contact_custom_column( $column, $post_id ) {
 	switch ( $column ) {
 		case 'message':
-			echo get_the_excerpt();
+			echo esc_attr( get_the_excerpt() );
 			break;
-
 		case 'email':
-			echo 'email address';
+			$email = get_post_meta( $post_id, '_contact_email_value_key', true );
+			echo esc_attr( $email );
 			break;
 		default:
 			break;
 	}
+}
+
+/** Contact meta boxes */
+function sunset_contact_add_meta_box() {
+	add_meta_box(
+		'contact_email',
+		'User Email',
+		'sunset_contact_email_callback',
+		'sunset-contact',
+		'side'
+	);
+}
+
+function sunset_contact_email_callback( $post ) {
+	wp_nonce_field( 'sunset_save_contact_email_data', 'sunset_contact_email_meta_box_nonce' );
+
+	$value = get_post_meta( $post->ID, '_contact_email_value_key', true );
+
+	echo '<label for="sunset_contact_email_field">User Email Adress: </label>';
+	echo '<input type="email" id="sunset_contact_email_field" name="sunset_contact_email_field" value="' . esc_attr( $value ) . '" size="25" />';
+}
+
+function sunset_save_contact_email_data( $post_id ) {
+	if ( ! isset( $_POST['sunset_contact_email_meta_box_nonce'] ) ) {
+		return;
+	}
+
+	if ( ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['sunset_contact_email_meta_box_nonce'] ) ), 'sunset_save_contact_email_data' ) ) {
+		return;
+	}
+
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
+
+	if ( ! isset( $_POST['sunset_contact_email_field'] ) ) {
+		return;
+	}
+
+	$email = sanitize_text_field( wp_unslash( $_POST['sunset_contact_email_field'] ) );
+
+	update_post_meta( $post_id, '_contact_email_value_key', $email );
 }
